438a98c30d9d019fa1b63975ec7dd28df346d88a
yunling2003 <yunling2003@aliyun.com>
2018-07-08 21:59:40 +0800

    Add page header test.

---
+++ client/__test__/component/PageHeader.spec.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import PageHeader from '../../src/components/PageHeader.js'
+import { mount } from 'enzyme'
+
+const setup = () => {
+    const deskState = {
+        viewportWidth: 800,
+        menuVisible: true
+    }
+    const mobileState = {
+        viewportWidth: 500,
+        menuVisible: true
+    }
+    const wrapper = mount(<PageHeader />)
+    return {
+        deskState,
+        mobileState,
+        wrapper
+    }
+}
+
+describe('PageHeader', () => {
+    const { mobileState, deskState, wrapper } = setup()
+
+    it('PageHeader should render as desktop mode', () => {
+        wrapper.setState(deskState)
+        expect(wrapper.find('.layout').exists()).toBeTruthy()
+        expect(wrapper.find('.visible').exists()).toBeFalsy()
+    })
+
+    it('PageHeader should render as mobile mode', () => {
+        wrapper.setState(mobileState)
+        expect(wrapper.find('.layout').exists()).toBeTruthy()
+        expect(wrapper.find('.visible').exists()).toBeTruthy()
+    })
+
+    it('Simulate click menu icon', () => {
+        wrapper.setState(mobileState)
+        expect(wrapper.find('.visible').exists()).toBeTruthy()
+        wrapper.find('Icon').findWhere(x => x.prop('type') === 'bars').simulate('click')
+        expect(wrapper.find('.hide').exists()).toBeTruthy()
+        expect(wrapper.find('.visible').exists()).toBeFalsy()
+        wrapper.find('Icon').findWhere(x => x.prop('type') === 'bars').simulate('click')
+        expect(wrapper.find('.hide').exists()).toBeFalsy()
+        expect(wrapper.find('.visible').exists()).toBeTruthy()       
+    })
+})

c62ec457d17eb3ca1166aa68ed8c6d92bd7c361a
yunling2003 <yunling2003@aliyun.com>
2020-01-03 20:58:57 +0800

    change beian web site link.

--- client/src/components/shared/PageFooter.js
+++ client/src/components/shared/PageFooter.js
@@ -3,7 +3,7 @@ import CSSModules from 'react-css-modules'
 import styles from './PageFooter.css'
 
 const PageFooter = (state = {}) => (
-    <p styleName='extendToBottom'>Copyright ©2019 BlogNx <a href="http://www.miitbeian.gov.cn/" target="_blank">沪ICP备19002267号</a></p>
+    <p styleName='extendToBottom'>Copyright ©2019 BlogNx <a href="http://www.beian.miit.gov.cn" target="_blank">沪ICP备19002267号</a></p>
 )
 
 export default CSSModules(PageFooter, styles)
